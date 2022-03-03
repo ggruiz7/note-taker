aa6577b53a4751eae27ad09484671ae6e36b3394
Gabriel Ruizz <garciagabriel1945@gmail.com>
2022-03-02 19:00:21 -0800

    update html route

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ const htmlRoutes = require("./routes/htmlRoutes");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("/public"));
+app.use(express.static(__dirname + "/public"));
 app.use("/api", apiRoutes);
 app.use("/", htmlRoutes);
 
