54086b062ba6b0b33e21561bac1bef7fb925b6d1
Gabriel Ruizz <garciagabriel1945@gmail.com>
2022-03-02 19:04:00 -0800

    updating path again

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -19,12 +19,12 @@ const path = require("path");
 const router = require("express").Router();
 
 router.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "/public/notes.html"));
+  res.sendFile(path.join(__dirname, "../public/notes.html"));
 });
 
 // if no matching route is found, default to home
 router.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "/public/index.html"));
+  res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
 module.exports = router;
